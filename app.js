3c3f390dd7ddb16e41a0ba398eec8bc9db00367f
alhz-sadd <alhz26pt08053tb@alinrmail.com>
2026-09-19 20:51:27 +0800

    2222

--- app.js
+++ app.js
@@ -22,11 +22,6 @@ class AppBootHook {
     this.app.config.proxy = true;
     this.app.config.maxProxyCount = 1;
   }
-  
-  // 在中间件最顶层拦截并打印 IP 和 Headers
-  async configDidLoad() {
-    this.app.config.coreMiddleware.unshift('ipTracker');
-  }
 
   async beforeStart() {
     const app = this.app;
